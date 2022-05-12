4d8751f47ef43d3d0eb1ed80a56e0aeaf03bf7a2
Gurmannat Sohal <g_sohal@ch.iitr.ac.in>
2022-05-13 00:18:26 +0530

    set existing keys

--- Desktop/GPG_SSH.sh
+++ Desktop/GPG_SSH.sh
@@ -3,6 +3,15 @@ read -p "Do you want to use existing key (e) or generate a new one (n)? " new
 if [ $new == "e" ];
 then
   gpg --list-secret-keys --keyid-format=long
+  read -p "Do you want to set any key? (y/n) " want
+
+ if [ $want == "y" ];
+  then
+   read -p "Paste your key ID: " paste
+   git config --global user.signingkey $paste
+  else
+  echo "Bye"
+  fi
 else
   read -p "Are you on version 2.1.17 or greater? (y/n) " version
   if [ $version == "y" ];
@@ -12,8 +21,12 @@ else
     gpg --default-new-key-algo rsa4096 --gen-key
   fi
   keygen=$(gpg --list-secret-keys --keyid-format=long|awk '/sec/{if (length($2)>0) print $2}');
+  IFS=''
+  read -a arr <<< "$keygen"
+  len=${#arr[@]}
+  fullKey=${arr[len-1]}
   IFS='/'  
-  read -a keyarr <<<"$keygen"
+  read -a keyarr <<< "$fullKey"
   key=${keyarr[1]}
   echo "Paste the following ASCII code on github"
   gpg --armor --export $key
